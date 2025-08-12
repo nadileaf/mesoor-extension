62bbca5924ead641c65acd0fc389024dfa2c4dc0
chenjiabin <>
2025-08-12 15:58:30 +0800

    feat: 添加socket连接开关功能、简历同步支持页面变动
    1. 新增VITE_ENABLE_SOCKET_CONNECTION环境变量用于在background.js控制socket连接
    2.降低html解析页面等待时间

--- src/content/sync-html.ts
+++ src/content/sync-html.ts
@@ -6,7 +6,7 @@ import { processHTML } from '../utils/syncReceiveResumeUtil';
 
 console.log('sync-html: done');
 async function run() {
-  await delay(8000); // 抓取的时候页面可能没有完全加载完，所以等待一段时间再抓取
+  await delay(2000); // 抓取的时候页面可能没有完全加载完，所以等待一段时间再抓取
   const html = await processHTML();
   const msg: IViewResumeMessage = {
     requestId: uuid(),
